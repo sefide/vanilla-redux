cf234adb1138d467ca36f80b04dbeaacb04d18c6
sefide <sefide@naver.com>
2020-12-15 23:04:33 +0900

    feat: toolkit - createAction

--- src/store.js
+++ src/store.js
@@ -1,28 +1,18 @@
 import { createStore } from "redux";
+import { createAction } from "@reduxjs/toolkit";
 
-const ADD = "ADD";
-const DELETE = "DELETE";
-
-const addToDo = (text) => {
-    return {
-        type: ADD,
-        text
-    };
-};
-
-const deleteToDo = (id) => {
-    return {
-        type: DELETE,
-        id: parseInt(id)
-    };
-};
+// addToDo, deleteToDo는 function
+// .type은 텍스트
+// addToDo() 할 경우 type과 payload 객체를 반환
+const addToDo = createAction("ADD");
+const deleteToDo = createAction("DELETE");
 
 const reducer = (state = [], action) => {
     switch (action.type) {
-        case ADD :
-            return [{text : action.text, id : Date.now()}, ...state];
-        case DELETE :
-            return state.filter(toDo => toDo.id !== action.id);
+        case addToDo.type :
+            return [{text : action.payload, id : Date.now()}, ...state];
+        case deleteToDo.type :
+            return state.filter(toDo => toDo.id !== action.payload);
         default :
             return state;
     }
