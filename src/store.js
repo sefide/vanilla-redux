bf4e442ac3517d23555655b0ed7cdcfda08c5a36
sefide <sefide@naver.com>
2020-12-16 22:12:07 +0900

    refactor: remove create old reducer

--- src/store.js
+++ src/store.js
@@ -7,17 +7,6 @@ import { createAction, createReducer } from "@reduxjs/toolkit";
 const addToDo = createAction("ADD");
 const deleteToDo = createAction("DELETE");
 
-// const reducer = (state = [], action) => {
-//     switch (action.type) {
-//         case addToDo.type :
-//             return [{text : action.payload, id : Date.now()}, ...state];
-//         case deleteToDo.type :
-//             return state.filter(toDo => toDo.id !== action.payload);
-//         default :
-//             return state;
-//     }
-// }
-
 // initialState, builderCallback
 const reducer = createReducer([], {
     // action.Type
