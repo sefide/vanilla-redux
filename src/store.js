49ae8502317dee984e5d3922e0244720e62a6733
sefide <sefide@naver.com>
2020-12-16 22:20:28 +0900

    feat: toolkit - configureStore

--- src/store.js
+++ src/store.js
@@ -1,5 +1,5 @@
 import { createStore } from "redux";
-import { createAction, createReducer } from "@reduxjs/toolkit";
+import { createAction, createReducer, configureStore } from "@reduxjs/toolkit";
 
 // addToDo, deleteToDo는 function
 // .type은 텍스트
@@ -16,7 +16,7 @@ const reducer = createReducer([], {
     [deleteToDo] : (state, action) => state.filter(toDo => toDo.id !== action.payload)
 });
 
-const store = createStore(reducer);
+const store = configureStore({reducer});
 
 export const actionCreators = {
     addToDo,
