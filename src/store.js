19a2f4562de2d1123f4fc0b188636230167fa051
sefide <sefide@naver.com>
2020-12-17 22:43:09 +0900

    refactor: remove old code

--- src/store.js
+++ src/store.js
@@ -7,15 +7,6 @@ import { createAction, createReducer, configureStore, createSlice } from "@redux
 const addToDo = createAction("ADD");
 const deleteToDo = createAction("DELETE");
 
-// initialState, builderCallback
-// const reducer = createReducer([], {
-//     // action.Type
-//     [addToDo] : (state, action) => {
-//         state.push({text : action.payload, id : Date.now()}); // you can mutate, but don't return
-//     },
-//     [deleteToDo] : (state, action) => state.filter(toDo => toDo.id !== action.payload)
-// });
-
 const toDos = createSlice({
     name: "toDosReducer", 
     initialState: [],
